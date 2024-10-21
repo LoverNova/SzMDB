5645803633df49b34543e4c9a5cb0d0454b28560
Szeremi Attila <szeremi.attila.1031@gr-szechenyi.inside>
2024-10-21 12:20:25 +0200

    created a new main page and added a acces route to it

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     switch($path){
         case "" :
             
+            require_once("./view/cimlap.php")
+            break;
         default:
             echo("404 - page not found <a href = ''>Ugrás a címlapra</a>");
             break;
