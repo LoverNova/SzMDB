7d6164203e8589b38cdd0d525a4563dce0157523
Nova <153313378+LoverNova@users.noreply.github.com>
2024-10-21 11:56:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 
     switch($path){
-        case "/13_F1/2024_10_01/Ingatlanok/" :
+        case "" :
             
         default:
             echo("404 - page not found <a href = '/13_F1/2024_10_01/Ingatlanok/'>Ugrás a címlapra</a>");
