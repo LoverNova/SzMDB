a3815c00df369a87b318b21352e81c435f68b2ae
LoverNova <szeremiati@gmail.com>
2024-12-03 09:43:02 +0100

    Moved the css and json to the public folder. The href is a bit long now but at least it works :3

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <?php
     $path = parse_url($_SERVER["REQUEST_URI"])["path"];
     // Change default path to the final location
-    $data = json_decode(file_get_contents("./view/json/movies.json", true) ,true);
     $defaultPath = '/SzMDB/';
     switch($path){
         case $defaultPath:
-            require_once("./view/cimlap.php");
-            echo($data[0]["nev"]);
+            require_once("./view/cimlap.html");
             break;
         default:
-            require_once("./view/404.php");
+            require_once("./view/404.html");
             break;
     }
 ?>
